7d612d754115c35ac36c2116b111861a0c5702a4
anshu <anshhufx@gmail.com>
2026-05-24 21:53:21 +0530

    fix scale

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
           alt="img"
         />
       </div>
-      <div className="h-[28rem] w-96 absolute top-32 right-96 ">
+      <div className="h-[28rem] w-96 absolute top-32 right-96">
         {/* top-img */}
         <img
           className=" h-full w-full object-cover"
@@ -23,12 +23,18 @@ const App = () => {
         <h1>OF</h1>
         <h1>SPORTS</h1>
       </div>
-      <div className="w-full px-7 font-semibold absolute bottom-5 flex justify-between tracking-tight">
-        <p className="cursor-pointer">Terms of Service</p>
-        <p className="cursor-pointer">Privacy Policy</p>
-        <p className="cursor-pointer">Privacy Settings</p>
+      <div className="w-full px-7 font-semibold absolute bottom-5 flex justify-between tracking-tight ">
+        <p className="cursor-pointer py-2 px-4 border border-transparent hover:border-neutral-950 rounded-full active:scale-95 transition-transform duration-200">
+          Terms of Service
+        </p>
+        <p className="cursor-pointer py-2 px-4 border border-transparent hover:border-neutral-950 rounded-full active:scale-95 transition-transform duration-200">
+          Privacy Policy
+        </p>
+        <p className="cursor-pointer py-2 px-4 border border-transparent hover:border-neutral-950 rounded-full active:scale-95 transition-transform duration-200">
+          Privacy Settings
+        </p>
       </div>
-      <div className="w-32 right-24 absolute top-32 cursor-pointer">
+      <div className="w-32 right-24 absolute top-32 cursor-pointer hover:-translate-y-0.5 transition-transform duration-200">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/1/1c/Adidas-group-logo-fr.svg"
           alt=""
